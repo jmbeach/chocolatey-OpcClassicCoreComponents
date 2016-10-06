a32bffa2d647a5f358cdb22121d70248667341c8
jmbeach <jaredbeachdesign@gmail.com>
2016-10-06 13:56:12 -0500

    init

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$name= 'OpcClassicCoreComponents'
+$url        = 'https://www.dropbox.com/s/wc3ttini4t0tm91/opc-core-componentsx86.msi?dl=1'
+$url64      = 'https://www.dropbox.com/s/v1ku7cpqh09qrnf/opc-core-componentsx64.msi?dl=1'
+$silent = '/quiet'
+ 
+Install-ChocolateyPackage $name 'msi' $silent $url $url64
